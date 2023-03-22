775d4a83da801b20efced26fcf3d1b036237e5cf
vogier <vogier@student.ethz.ch>
2023-03-22 10:59:45 +0100

    No clear license on Pratt and Tezcan, removing

--- references/pratt/aes_cuda.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void encrypt_cuda(const uint8_t *data, uint8_t *key, uint8_t *rseed, uint32_t numblock);
-void encrypt_cuda_io(const uint8_t *inparray, uint8_t *outarray, uint8_t *key, uint8_t *rseed, uint32_t numblock);
