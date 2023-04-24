6d0c6fd72bde6f7035398988616ad1b52f3c4e17
hi@abertschi.ch <hi@abertschi.ch>
2023-04-25 00:57:35 +0200

    bench: add marker instructions for aarch64 only

--- enc_cuda/src/cca_benchmark.h
+++ enc_cuda/src/cca_benchmark.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #define STR(s) #s
+#if defined(__x86_64__) || defined(_M_X64)
+#define CCA_MARKER(marker)
+#else
 #define CCA_MARKER(marker) __asm__ volatile("MOV XZR, " STR(marker))
+#endif
 
 #define CCA_MARKER_CPU_ENC CCA_MARKER(0x80)
 #define CCA_MARKER_CPU_DEC CCA_MARKER(0x81)
